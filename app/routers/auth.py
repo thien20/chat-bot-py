d925f7633187c002d189affe45671d298b04f3fa
thien <thienhtt20@gmail.com>
2025-01-13 23:22:43 +0700

    code adjustment and reconfig/documenting refresh token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, HTTPException, status, APIRouter
+from fastapi import Depends, HTTPException, status, APIRouter, Response
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
@@ -24,22 +24,41 @@ def get_db():
         db.close()
 
 @router.post("/token", response_model=Token)
-async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
+async def login_for_access_token(response: Response,
+                                form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                db: Session = Depends(get_db)):
     """
-    DESS: This function is to for user login and get the access token
-    PROBLEM - SOLVED: 
+    - DECS: This function is to for user `LOGIN` and get the access token
+    - PROBLEM - SOLVED: 
     - When you declare other function parameters that are not part of the path parameters, 
     they are automatically interpreted as "query" parameters.
     - STATUS: DONE
+
+    NOTE: We should not return the refresh token here. 
+        - HTTP-only Cookies (recommended): Store the refresh token in a Secure, SameSite, and HTTP-only cookie to mitigate XSS attacks. 
+        Use the Secure flag if your application uses HTTPS. 
+        - Avoid storing refresh tokens in localStorage or sessionStorage.
     """
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
     
-    access_token = create_access_token(data={"sub": user.username}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    access_token = create_access_token(
+                                    data={"sub": user.username}, 
+                                    expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+                                    )
     
-    return {"access_token": access_token, "token_type": "bearer"}
+    refresh_token = create_refresh_token(data={"sub": user.username})
+    # Set refresh token in a secure HTTP-only cookie
+    response.set_cookie(
+        key="refresh_token",
+        value=refresh_token,
+        httponly=True,
+        secure=True,
+        samesite="Strict",
+    )
 
+    return {"access_token": access_token, "token_type": "bearer"} 
 
 @router.post("/refresh", response_model=Token)
 async def refresh_access_token(
